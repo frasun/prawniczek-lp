ed18412a22ee25d400f6aea96380d057c02b674f
fraz <fraz@o2.pl>
2023-05-02 14:40:02 +0200

    add critical css and text compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import critters from "astro-critters";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()]
+  integrations: [tailwind(), image(), critters(), compress({
+    img: false,
+    svg: false
+  }),]
 });
